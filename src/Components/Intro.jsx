0d1dc0baf32d2502954ba7534a65ebcdff81d79c
geraldkisilu <muligerald6@gmail.com>
2024-10-22 09:27:13 +0300

    commit

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import logo from '../Components/Assets/Goalhublogo.png'; // Assuming this is your logo
-import './Intro.css'; // Add this for your styling
+import logo from '../Components/Assets/Goalhublogo.png'; // Make sure this path is correct
+import './Intro.css';
 
 function Intro() {
     return (
         <div className='intro'>
-            <img src={logo} className="card-img-top" alt="Card Image" />
-            {/* Card Component */}
-            <div className="card" style={{ width: '18rem' }}>
-                
-                <div className="card-body">
-                    <h5 className="card-title">Welcome to Goal Hub</h5>
-                    <p className="card-text">
-                        This is a training guidance app for kids to view their schedules, times, and activities. Enjoy the journey!
-                    </p>
-                    <a href="#" className="btn btn-primary">Get Started</a>
-                </div>
-            </div>
+            <img src={logo} alt="Goal Hub Logo" className="logo" />
         </div>
     );
 }
